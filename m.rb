145a0e27d8a56b4fa8d94597631c86ed122a8fc8
Adam La Rosa <adam.larosa@gmail.com>
2020-03-14 21:10:28 -0700

    initial commit

---
+++ m.rb
@@ -0,0 +1,8 @@
+require 'json'
+require 'rest-client'
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
+http = "https://www.mountainproject.com/data/get-routes-for-lat-lon?lat=40.03&lon=-105.25&maxDistance=10&minDiff=5.6&maxDiff=5.10&key=200667896-4544667becb81f9a916ea81a1fcb69c5"
+
