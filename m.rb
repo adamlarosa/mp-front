d5f0eafbd8248e4debe3a9312eb0ec74c088e910
Adam La Rosa <adam.larosa@gmail.com>
2020-03-14 21:13:15 -0700

    data fetch

--- m.rb
+++ m.rb
@@ -6,3 +6,8 @@ require 'pry'
 
 http = "https://www.mountainproject.com/data/get-routes-for-lat-lon?lat=40.03&lon=-105.25&maxDistance=10&minDiff=5.6&maxDiff=5.10&key=200667896-4544667becb81f9a916ea81a1fcb69c5"
 
+new = JSON.parse(RestClient.get(http))
+
+binding.pry
+
+puts 'eof'
